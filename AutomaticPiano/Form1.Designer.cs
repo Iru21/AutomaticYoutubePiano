6677ebf41b180520aa066b9c8f80a168defec1f3
Iru21 <33987046+Iru21@users.noreply.github.com>
2021-06-29 20:31:40 +0200

    Add project files.

---
+++ AutomaticPiano/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace AutomaticPiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.SystemColors.Control;
+            this.Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Button.FlatAppearance.BorderSize = 2;
+            this.Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Button.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Button.Location = new System.Drawing.Point(9, 235);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(325, 59);
+            this.Button.TabIndex = 11;
+            this.Button.Text = "START";
+            this.Button.UseVisualStyleBackColor = false;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Wingdings", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lblOutput.Location = new System.Drawing.Point(213, 190);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(29, 30);
+            this.lblOutput.TabIndex = 9;
+            this.lblOutput.Text = "6";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Wingdings", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lblInput.Location = new System.Drawing.Point(208, 28);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(83, 59);
+            this.lblInput.TabIndex = 8;
+            this.lblInput.Text = "7";
+            // 
+            // Input
+            // 
+            this.Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Input.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Input.Location = new System.Drawing.Point(12, 88);
+            this.Input.MaxLength = 500;
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Input.Size = new System.Drawing.Size(476, 91);
+            this.Input.TabIndex = 6;
+            this.Input.Text = "858567336545112234456789550989558767336545118765";
+            this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.White;
+            this.numericUpDown1.Location = new System.Drawing.Point(306, 196);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatAppearance.BorderSize = 0;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 188);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 28);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Loop";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(469, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 24);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.BackColor = System.Drawing.SystemColors.Control;
+            this.stop_btn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.stop_btn.FlatAppearance.BorderSize = 0;
+            this.stop_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stop_btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.stop_btn.Location = new System.Drawing.Point(340, 235);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(146, 59);
+            this.stop_btn.TabIndex = 16;
+            this.stop_btn.Text = "STOP";
+            this.stop_btn.UseVisualStyleBackColor = false;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(242, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = " BPM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Automatic Youtube Piano";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Open Piano";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Input";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 325);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoPiano";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AutomaticPiano/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace AutomaticPiano
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        bool repeat;
+        Thread t;
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = Input;
+        }
+
+    protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            Input.Text = System.Text.RegularExpressions.Regex.Replace(Input.Text, "[^0-9JLjl]", "");
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            t = new Thread(new ThreadStart(start));
+            t.Start();
+        }
+
+        private void start()
+        {
+            if (Input.Text.Length == 0)
+            {
+                System.Media.SystemSounds.Beep.Play();
+                this.ActiveControl = Input;
+            }
+            Thread.Sleep(1000);
+            SendKeys.SendWait(" ");
+            Thread.Sleep(100);
+            if (repeat)
+            {
+                while (repeat)
+                {
+                    int bpm = 60000 / (Convert.ToInt32(numericUpDown1.Value) * 4);
+
+                    for (int i = 0; i < Input.Text.Length; ++i)
+                    {
+                        string additional = null;
+                        bool hasAdditional = false;
+                        if (System.Text.RegularExpressions.Regex.IsMatch(Convert.ToString(Input.Text[i + 1]), "J") || System.Text.RegularExpressions.Regex.IsMatch(Convert.ToString(Input.Text[i + 1]), "L"))
+                        {
+                            additional = Convert.ToString(Input.Text[i + 1]);
+                            hasAdditional = true;
+                        }
+                        play(bpm, Convert.ToString(Input.Text[i]), hasAdditional, additional);
+
+                    }
+                }
+            }
+            else
+            {
+                int bpm = 60000 / (Convert.ToInt32(numericUpDown1.Value) * 4);
+
+                for (int i = 0; i < Input.Text.Length; ++i)
+                {
+                    string additional = null;
+                    bool hasAdditional = false;
+                    if (Input.Text.Length != i + 1)
+                    {
+                        if (System.Text.RegularExpressions.Regex.IsMatch(Convert.ToString(Input.Text[i + 1]), "J") || System.Text.RegularExpressions.Regex.IsMatch(Convert.ToString(Input.Text[i + 1]), "L"))
+                        {
+                            additional = Convert.ToString(Input.Text[i + 1]);
+                            hasAdditional = true;
+                        }
+                    }
+                    play(bpm, Convert.ToString(Input.Text[i]), hasAdditional, additional);
+                }
+                Thread.Sleep(300);
+                SendKeys.SendWait(" ");
+                System.Media.SystemSounds.Beep.Play();
+                t.Abort();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                Clipboard.SetText(Input.Text);
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) repeat = true;
+            else repeat = false; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            if(t.IsAlive)
+            {
+                t.Abort();
+                System.Media.SystemSounds.Beep.Play();
+            }
+        }
+
+        private void play(int bpm, string note, bool hasAdditional, string additional)
+        {
+            Thread.Sleep(bpm);
+            SendKeys.SendWait(note);
+            if(hasAdditional)
+            {
+                Thread.Sleep(10);
+                SendKeys.SendWait(additional);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=3gZC5763wYk");
+        }
+    }
+}
